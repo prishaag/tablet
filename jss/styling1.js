2fd0d881ce9127fe431ce8ac5cfc2fa02a374cf3
prishaag <prishaag@umich.edu>
2023-11-05 13:11:24 -0500

    everything

---
+++ jss/styling1.js
@@ -0,0 +1,17 @@
+document.addEventListener('click', function (event) {
+    if (event.target.tagName === 'IMG') {
+      const overlay = document.createElement('div');
+      overlay.classList.add('overlay');
+      const image = document.createElement('img');
+      image.src = event.target.src;
+      overlay.appendChild(image);
+      document.body.appendChild(overlay);
+      overlay.addEventListener('click', function () {
+        document.body.removeChild(overlay);
+      });
+    }
+  });
+
+
+
+  
