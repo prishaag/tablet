21124f64f382c46c5b198151c47a9dd02a41a4f0
prishaag <prishaag@umich.edu>
2023-11-05 14:14:08 -0500

    stuff

--- jss/styling1.js
+++ jss/styling1.js
@@ -28,8 +28,8 @@ document.addEventListener('click', function (event) {
   }
   
   function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
   }
   
 
